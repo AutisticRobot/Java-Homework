d91bb21a0f234f62f9e5c8adf6034b6993bf37a7
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-05-04 21:26:52 -0700

    Started Sales Analysis assinment.

---
+++ Lab9/SalesAnalysisDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+ * Sales Analysis Demo shows off the capibilites of the Sales analysis java class.
+ * @author Tyler M. Kormann
+ * @version v1.0
+ * @since 5/4/2023, Tyler M. Kormann, v1.0
+ */
+public class SalesAnalysisDemo {
+    public static void main(String[] args) {
+        Scanner getInput = new Scanner(System.in);
+        String file;
+
+        System.out.print("\n\nEnter the path to the SalesData.txt file: ");//asks the user for the text file for input. two line brakes for styling.
+        file = getInput.next();
+
+        
+    }
+    
+}
