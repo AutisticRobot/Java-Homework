1a9fc1c73c901848ecd273ebe481265270847b03
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-05-25 12:50:59 -0700

    Started RegistrationMethods.java

--- FinalLab/RegistrationMethods.java
+++ FinalLab/RegistrationMethods.java
@@ -1,11 +1,47 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class RegistrationMethods implements RegistrationMethodsInterface
 {
+    private Scanner getInput;
+    private String inputFileName;
+    private String outputFileName;
+    private String binFileName;
+
+    
 
     @Override
     public void setFileNames()
     {
+        String filePath = "";
+        getInput = new Scanner(System.in);
+
+        while(!new File(filePath).exists())
+        {
+            System.out.print("Enter the path of the csv file to be processed: ");
+            filePath = getInput.next();
+        }
+        inputFileName = filePath;
+        filePath = "";
+
+        while(!new File(filePath).exists())
+        {
+            System.out.print("Enter the path of the output file to be processed: ");
+            filePath = getInput.next();
+        }
+        outputFileName = filePath;
+        filePath = "";
+
+        while(!new File(filePath).exists())
+        {
+            System.out.print("Enter the path of the binary output file to be processed: ");
+            filePath = getInput.next();
+        }
+        binFileName = filePath;
+        filePath = "";
+
+        getInput.close();
+
     }
 
     @Override
